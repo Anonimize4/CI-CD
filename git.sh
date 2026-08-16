b6fe0eb665427f268192679745f7bde429b0b3a2
Anonism123 <samuelbelay81@gmail.com>
2026-08-16 06:52:26 -0400

    adding commit

--- git.sh
+++ git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "Hahaha... whait till finish bro!"
+echo "I have never been satisfied... but, one day will be done!"
 git add . && git commit -m "adding commit" && git push origin main 
